e6ebb7edc6f411e923d4de5055beb5000b483b16
Chris <chris@chocolatey.io>
2024-07-16 14:35:10 -0400

    Added Start-CCMDeploymentPlan Function
    In this commit we added the Start-CCMDeploymentPlan function to allow starting deployments through the API.

---
+++ source/public/DeploymentPlans/Start-CCMDeploymentPlan.ps1
@@ -0,0 +1,36 @@
+function Start-CCMDeploymentPlan {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory)]
+        [ArgumentCompleter({
+                param(
+                    $Command,
+                    $Parameter,
+                    $WordToComplete,
+                    $CommandAst,
+                    $FakeBoundParams )
+        
+                $CompletionResults = (Get-CCMDeploymentPlan).Name
+
+                if ($WordToComplete) {
+                    $CompletionResults.Where{ $_ -match "^$WordToComplete" }
+                }
+                else {
+                    $CompletionResults
+                }
+            })]
+        [String]
+        $DeploymentName
+        
+    )
+
+    process {
+
+        $Deployment = Get-CCMDeploymentPlan -Filter $DeploymentName
+    
+        #Start Deployment Plan
+        $null = Invoke-CCMApi -Method "POST" -Slug "DeploymentPlans/Start" -Body @{
+            id = $Deployment.id
+        }
+    }
+}
