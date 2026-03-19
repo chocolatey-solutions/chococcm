4c7835be223445aa4595534676605229949c8e67
Stephen Valdinger <stephen@chocolatey.io>
2026-03-19 16:24:03 -0400

    (maint) Improve query string helper
    converts New-CCMQueryString to use a more robust parsing
    method to create accurate query strings

--- source/private/New-CCMQueryString.ps1
+++ source/private/New-CCMQueryString.ps1
@@ -1,23 +1,43 @@
-function New-CCMQueryString
-{
-    #Shamelessly taken from https://powershellmagazine.com/2019/06/14/pstip-a-better-way-to-generate-http-query-strings-in-powershell/
+function New-CCMQueryString {
+    <#
+    .SYNOPSIS
+    Turn a hashtable into a URI querystring
+    
+    .DESCRIPTION
+    Turn a hashtable into a URI querystring
+    
+    .PARAMETER QueryParameter
+    The hashtable to transform
+    
+    .EXAMPLE
+    New-CCMQueryString -QueryParameter @{ Animal = 'Dog'; Breed = 'Labrador'; Name = 'Dilbert'}
+    
+    .EXAMPLE
+    New-CCMQueryString -QueryParameter @{ Animal = 'Dog'; Breed = 'Labrador', 'Retriever'; Name = 'Dilbert'}
+    
+    .EXAMPLE
+    New-CCMQueryString -QueryParameter ([ordered]@{ Animal = 'Dog'; Breed = 'Labrador', 'Retriever'; Name = 'Dilbert'})
+    
+    .NOTES
+    Shamelessly taken from https://powershellmagazine.com/2019/06/14/pstip-a-better-way-to-generate-http-query-strings-in-powershell/
+    #>
     [CmdletBinding()]
     param 
     (
         [Parameter(Mandatory = $true)]
-        [Hashtable]
+        [System.Collections.IDictionary]
         $QueryParameter
     )
-    # Add System.Web
-    Add-Type -AssemblyName System.Web
-    
-    # Create a http name value collection from an empty string
-    $nvCollection = [System.Web.HttpUtility]::ParseQueryString([String]::Empty)
-    
-    foreach ($key in $QueryParameter.Keys)
-    {
-        $nvCollection.Add($key, $QueryParameter.$key)
+    $pairs = foreach ($key in $QueryParameter.Keys) {
+        $encodedKey = [System.Uri]::EscapeDataString($key)
+        if ($QueryParameter[$key].GetType().ImplementedInterfaces.Contains([System.Collections.ICollection])) {
+            $encodedValue = [System.Uri]::EscapeDataString($QueryParameter[$key] -join ',')
+        }
+        else {
+            $encodedValue = [System.Uri]::EscapeDataString([string]$QueryParameter[$key])
+        }
+        "$encodedKey=$encodedValue"
     }
-    
-    return $nvCollection.ToString()
+
+    return $pairs -join '&'
 }
